beea086bac3eb105f77c269cee77e6e25fb63314
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-01-14 22:05:40 +0100

    a

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function check() {
+    var allisdark = true
+    if(allisdark == false) {
+        document.getElementById("ilcentro").style.backgroundColor = 'black';
+    }
+    if (allisdark == true) {
+        document.getElementById("ilcentro").style.backgroundColor = 'white';
+    }
+
+}
