71e6ca349d21282d7e4b91f7079eba3232d3ab65
StarNumber12046 <francesco.lumaca2009@gmail.com>
2021-04-15 17:38:59 +0200

    secreto

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+console.error("salve")
+
+
+function editheight() {
+    console.error("salve")
+    barra = document.getElementById("barra-laterale")
+    barra.style.height = screen.height;
+    console.log(screen.height)
+}
+
 
 function check() {
     var allisdark = true
-    if(allisdark == false) {
+    if (allisdark == false) {
         document.getElementById("ilcentro").style.backgroundColor = 'black';
     }
     if (allisdark == true) {
