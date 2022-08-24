5d5fd5a030782d61c709ad87951658574c1f7ed2
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2022-08-24 10:45:44 +0200

    added wakatime (not working graph) and projects list

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
     import "https://code.iconify.design/iconify-icon/1.0.0-beta.3/iconify-icon.min.js"
     import Skills from "./Skills.svelte"
     import Contacts from "./Contacts.svelte"
+    import Time from "./WakaTime.svelte"
+    import Projects from "./Projects.svelte"
 </script>
 <main class="font-mono">
-  <div class="flex grid-cols-2 w-auto">
+  <div class="inline-flex">
     <div class="w-15 col-span-1">
       <img class="h-15" src="https://starnumber.tk/logo.png" alt="logo">
     </div>
@@ -16,6 +18,8 @@
   <div class="m-5">
     <Skills></Skills>
     <Contacts></Contacts>
+    <Time></Time>
+    <Projects></Projects>
   </div>
   
 </main>
---
+++ src/Projects.svelte
@@ -0,0 +1,15 @@
+<script></script>
+<main>
+    <h1 class="text-4xl font-mono pt-4">My Projects</h1>
+    <div class="inline-flex">
+        <a href="//baracchinodellascuola.ga/" class="rounded-full p-1"><img class="rounded-full border-green-400 " src="//cdn.discordapp.com/icons/838727867428765766/a013f4e06d6cb7c62338841d460bb93d.png?size=48" alt="Baracchino della scuola"></a>
+        <a href="//github.com/StarNumber12046/starnumber12046.github.io" class="rounded-full border-green-400 p-1"><img class=" rounded-full border-green-400" width="52px" src="/logo.png" alt="My website"></a>
+    </div>
+</main>
+<style>
+    img {
+        border-radius: 100%;
+        border-width: 2px;
+        
+    }
+</style>
