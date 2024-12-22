b675228380add328f2a4e2bead9be2ce8a86f7ea
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-12-22 15:05:51 +0100

    Added vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -12,4 +14,6 @@ export default defineConfig({
     }),
     react(),
   ],
+
+  adapter: vercel(),
 });
