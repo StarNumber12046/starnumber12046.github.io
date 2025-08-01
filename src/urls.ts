66e5e08cb670b224cf6f7a675572eb0f16cb1e99
StarNumber <starnumber.official12046@gmail.com>
2025-08-01 20:54:24 +0200

    Add Misty Images from 1400914473971220604

--- src/urls.ts
+++ src/urls.ts
@@ -115,5 +115,6 @@ export const urls: string[] = [
     "https://8ek6ccfrg2.ufs.sh/f/EO7fVpuYmASc61CRX6ztY5EZHTjS4q0GWpdvoaXAyzicxCRK",
     "https://8ek6ccfrg2.ufs.sh/f/EO7fVpuYmAScJ1iP2GQmmISiDFWz5cdwBvXqNZf96Pubn3e7",
     "https://8ek6ccfrg2.ufs.sh/f/EO7fVpuYmAScvUSZHAEYplugsz2NtUr14bFWBVAO5f0emwLR",
-    "https://8ek6ccfrg2.ufs.sh/f/EO7fVpuYmAScCzx1HYqsS1GqkX2c6QmFPi84jgaUVYvzdEly"
+    "https://8ek6ccfrg2.ufs.sh/f/EO7fVpuYmAScCzx1HYqsS1GqkX2c6QmFPi84jgaUVYvzdEly",
+    "https://8ek6ccfrg2.ufs.sh/f/EO7fVpuYmAScANlc1NdmF7BE2PlRDtu4kVTQgXGiJSfcKn1W"
   ];
