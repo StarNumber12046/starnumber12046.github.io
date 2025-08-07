2eff4b535fd99dc93a0954bf3c7a2836b43fdb3d
StarNumber <starnumber.official12046@gmail.com>
2025-08-07 10:08:13 +0200

    Create misty.json.ts

---
+++ src/pages/misty.json.ts
@@ -0,0 +1,12 @@
+import { urls } from '../../urls.ts';
+
+export const GET: APIRoute = ({ request, redirect }) => {
+    
+    return new Response(JSON.stringify(urls), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Cache-Control": "no-store", // or something else if needed
+      },
+    });
+};
